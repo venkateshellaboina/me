03023d0bf59b31d222ac8a5b446df8a7c0830e89
Venkatesh Ellaboina <venkateshellaboina@Venkateshs-MacBook-Pro.local>
2022-04-10 14:30:31 -0400

    Projects link click fixed

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -7,8 +7,8 @@ class Portfolio extends Component {
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
+          <a href={projects.url} title={projects.title} target="_blank" rel="noopener noreferrer">
            <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
@@ -17,8 +17,8 @@ class Portfolio extends Component {
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
           </div>
+          </a>
         </div>
       })
     }
